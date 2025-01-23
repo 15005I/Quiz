ccbb42516f79bb330db231c2607456bcb0feb7c3
15005I <15005@portalcedi.edu.mx>
2025-01-23 09:35:05 -0600

    Quiz

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
+
 export class HomePage {
+  eventos: { nombre: string | null; fecha_i: string | null; ubicacion: string | null; confirmacion: boolean; fecha_f: string | null, nombreE: string |null, telefonoE: string | null }[] = [];
 
-  constructor() {}
+  evento = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    fecha_i: new FormControl('', Validators.required),
+    fecha_f: new FormControl('', Validators.required),
+    ubicacion: new FormControl('', Validators.required),
+    nombreE: new FormControl(''),
+    telefonoE: new FormControl('')
+  });
 
+  nEvento(){
+    if (this.evento.valid && this.evento.value.fecha_i! < this.evento.value.fecha_f! && this.evento.value.telefonoE! <= '10000000000') {
+      const nuevoEvento = {
+        nombre: this.evento.value.nombre!,
+        fecha_i: this.evento.value.fecha_i!,
+        fecha_f: this.evento.value.fecha_f!,
+        ubicacion: this.evento.value.ubicacion!,
+        nombreE: this.evento.value.nombreE!,
+        telefonoE: this.evento.value.telefonoE!,
+        confirmacion: false
+      };
+
+      this.eventos.push(nuevoEvento);
+
+      this.evento.reset();
+    }
+  }
+
+  constructor(){}
 }
